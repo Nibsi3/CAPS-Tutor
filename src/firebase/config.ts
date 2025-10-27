b143842899e99eaf50a7069af16504ac2a580355
Cameron .F <cameronfalck03@gmail.com>
2025-10-27 13:45:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3238820292-69b8f",
+  "appId": "1:709416001139:web:e5ede79f65173ca0069406",
+  "apiKey": "***REMOVED***",
+  "authDomain": "studio-3238820292-69b8f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "709416001139"
+};
