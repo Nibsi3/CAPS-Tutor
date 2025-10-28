545a0d1c394327aaf567a224175aba1b04bca51e
Cameron .F <cameronfalck03@gmail.com>
2025-10-28 12:40:58 +0000

    on the users icon in the top right when hovering on it it says the users

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -62,6 +62,9 @@ export function UserNav() {
           <DropdownMenuItem asChild>
              <Link href="/dashboard">Dashboard</Link>
           </DropdownMenuItem>
+           <DropdownMenuItem asChild>
+             <Link href="/dashboard/settings">Settings</Link>
+          </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={handleLogout}>
