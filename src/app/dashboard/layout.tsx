9160f4d35b6ce412dd6488583de63d4e40270886
Cameron .F <cameronfalck03@gmail.com>
2025-10-30 12:44:52 +0000

    can we also enable caching pages so that theyre prelaoded for next time?

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -63,6 +63,7 @@ export default function DashboardLayout({
                   <Link
                     key={item.label}
                     href={item.href}
+                    prefetch={true}
                     className={cn(
                       "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
                       isActive && "bg-accent text-primary"
